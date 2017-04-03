5646660317bee348165be44624241e936f48a767
Adamanth <villa@et.esiea.fr>
2017-04-03 15:37:56 +0200

    Add comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
-makeCacheMatrix <- function(original.matrix = matrix()) {
-        if (!is.matrix(original.matrix)) stop("It's not a matrix, please insert a matrix")
+## Create a list that contains functions to retrieve, add a matrix, as well as add its inverse or just retrieve it ...
+makeCacheMatrix <- function(x = matrix()) {
+        if (!is.matrix(x)) stop("It's not a matrix, please insert a matrix")
         inverse <- NULL
         set <- function(y) {
-                original.matrix <<- y
+                x <<- y
                 inverse <<- NULL
         }
-        get <- function() original.matrix
+        get <- function() x
         set.inverse <- function(solve) inverse <<- solve
         get.inverse <- function() inverse
         list(
@@ -14,14 +15,15 @@ makeCacheMatrix <- function(original.matrix = matrix()) {
                 set.inverse = set.inverse,
                 get.inverse = get.inverse)
 }
-cacheSolve <- function(cacheable.matrix, ...) {
-        invers <- cacheable.matrix$get.inverse()
+## Displays the inverse of the matrix or creates (with the "solve" function) this inverse and displays it
+cacheSolve <- function(c, ...) {
+        invers <- c$get.inverse()
         if(!is.null(invers)) {
                 message("Getting cached inverse matrix")
                 return(invers)
         }
-        invers <- solve(cacheable.matrix$get())
-        cacheable.matrix$set.inverse(invers)
+        invers <- solve(c$get())
+        c$set.inverse(invers)
         invers
 }
 R <- matrix(runif(16), ncol=4) 
